401d23d7ded049e2574abd056f31308fd31768a0
deepank <deepank@justdials-iMac-2.local>
2014-11-26 18:38:23 +0530

    added a test class

---
+++ DKTest1.h
@@ -0,0 +1,13 @@
+//
+//  DKTest1.h
+//  Git Test
+//
+//  Created by deepank on 26/11/14.
+//  Copyright (c) 2014 deepank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DKTest1 : NSObject
+
+@end
---
+++ DKTest1.m
@@ -0,0 +1,13 @@
+//
+//  DKTest1.m
+//  Git Test
+//
+//  Created by deepank on 26/11/14.
+//  Copyright (c) 2014 deepank. All rights reserved.
+//
+
+#import "DKTest1.h"
+
+@implementation DKTest1
+
+@end
